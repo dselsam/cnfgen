45aebb4db3e4dc708ec00492a31f6fb96583d536
Daniel Selsam <daniel.selsam@protonmail.com>
2018-01-24 10:14:06 -0500

    k-color parameter to allow skipping func clauses

--- cnfformula/families/coloring.py
+++ cnfformula/families/coloring.py
@@ -42,13 +42,13 @@ def GraphColoringFormula(G,colors,functional=True):
 
     if isinstance(colors,int) and colors>=0:
         colors = range(1,colors+1)
-    
+
     if not isinstance(list, collections.Iterable):
         ValueError("Parameter \"colors\" is expected to be a iterable")
-    
+
     # Describe the formula
     name="graph colorability"
-    
+
     if hasattr(G,'name'):
         col.header=name+" of graph:\n"+G.name+".\n\n"+col.header
     else:
@@ -63,7 +63,7 @@ def GraphColoringFormula(G,colors,functional=True):
         for color in colors:
             clause += [(True,'x_{{{0},{1}}}'.format(vertex,color))]
         col.add_clause(clause)
-        
+
         # unique color per vertex
         if functional:
             for (c1,c2) in combinations(colors,2):
@@ -77,7 +77,7 @@ def GraphColoringFormula(G,colors,functional=True):
             col.add_clause([
                 (False,'x_{{{0},{1}}}'.format(v1,c)),
                 (False,'x_{{{0},{1}}}'.format(v2,c))],strict=True)
-            
+
     return col
 
 
@@ -96,7 +96,7 @@ def EvenColoringFormula(G):
 
     Arguments
     ---------
-    G : networkx.Graph 
+    G : networkx.Graph
        a simple undirected graph where all vertices have even degree
 
     Raises
@@ -122,7 +122,7 @@ def EvenColoringFormula(G):
             return 'x_{{{0},{1}}}'.format(u,v)
         else:
             return 'x_{{{0},{1}}}'.format(v,u)
-    
+
     for (u, v) in enumerate_edges(G):
         F.add_variable(var_name(u, v))
 
@@ -133,7 +133,7 @@ def EvenColoringFormula(G):
             raise ValueError("Markstrom formulas requires all vertices to have even degree.")
 
         edge_vars = [ var_name(u,v) for u in neighbors(G,v) ]
-        
+
         for cls in CNF.equal_to_constraint(edge_vars,
                                            len(edge_vars)/2):
             F.add_clause(cls,strict=True)
@@ -156,6 +156,7 @@ class KColorCmdHelper(object):
         - `parser`: parser to load with options.
         """
         parser.add_argument('k',metavar='<k>',type=int,action='store',help="number of available colors")
+        parser.add_argument('func',metavar='<func>',type=int,action='store',help="functional constraints on colors")
         SimpleGraphHelper.setup_command_line(parser)
 
 
@@ -167,7 +168,7 @@ class KColorCmdHelper(object):
         - `args`: command line options
         """
         G = SimpleGraphHelper.obtain_graph(args)
-        return GraphColoringFormula(G,range(1,args.k+1))
+        return GraphColoringFormula(G,range(1,args.k+1), functional=args.func)
 
 
 
@@ -175,7 +176,7 @@ class KColorCmdHelper(object):
 class ECCmdHelper(object):
     name='ec'
     description='even coloring formulas'
-    
+
     @staticmethod
     def setup_command_line(parser):
         SimpleGraphHelper.setup_command_line(parser)
@@ -183,5 +184,5 @@ class ECCmdHelper(object):
 
     @staticmethod
     def build_cnf(args):
-        G = SimpleGraphHelper.obtain_graph(args) 
+        G = SimpleGraphHelper.obtain_graph(args)
         return EvenColoringFormula(G)
